172f32f404306257fed02f2b0d33c7f5bab8aa6e
WisnuKresnohadi <wisnukresnohadi@gmail.com>
2023-12-09 22:11:13 +0700

    testing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
             'password' => 'required',
             'cPassword' => 'required|same:password',
         ]);
-
+        
         User::create([
             'username' => $request->input('username'),
             'password' => bcrypt($request->input('password')),
